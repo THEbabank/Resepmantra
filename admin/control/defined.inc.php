241ba365132b3a14829f59f697d1d07e7d40a523
debby46 <septianti64@gmail.com>
2016-06-12 12:18:41 +0700

    admin 16

---
+++ admin/control/defined.inc.php
@@ -0,0 +1,22 @@
+<?php
+
+// Set username for administration..
+define ('USERNAME', 'x2one');
+
+// Set password for administration..
+define ('PASSWORD', 'x2one2x');
+
+// How many recipes per page in the admin area..
+define ('PER_PAGE', 30);
+
+// How many upload boxes to show in the admin area..
+// Should be divisible by 3 ideally..
+define ('ADMIN_UPLOAD_PIC_BOXES', 6);
+
+// Display recipe info box divs by default..
+define ('SHOW_RECIPE_INFO_BOXES', 3);
+
+// Enable/Disable action messages..
+define ('ENABLE_ACTION_MESSAGES', 1);
+
+?>
