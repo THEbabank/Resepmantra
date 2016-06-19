d228b630d389322e6ce8708a9949f5d9f615673c
debby46 <septianti64@gmail.com>
2016-06-19 21:46:56 +0700

    admin 161

---
+++ templates/about-us.tpl.php
@@ -0,0 +1,41 @@
+<!-- Start Body Area -->
+<div id="bodyArea">
+
+<div class="left">
+
+  <span class="headLeft"><?php echo $this->ABOUT_US_TXT; ?>:</span>
+
+  <p class="categories">
+  <?php
+  // This is your about us page text. You should remove this block of text and replace it with your own..
+  // You can use any HTML here or PHP if you are advanced enough..
+  ?>
+  resepmantra.com Adalah sebuah website tutorial/edukasi yang membahas tentang Masak-memasak, yaitu masakan Tradisional Indonesia.
+  </p>
+
+</div>
+
+<div class="right">
+
+  <span class="headRightMid"><?php echo $this->MOST_POPULAR_TXT; ?>:</span>
+  
+  <p class="rightP">
+    <?php echo $this->MOST_POPULAR; ?>
+  </p>
+
+
+  <span class="headRight"><?php echo $this->LATEST_RECIPES_TXT; ?>:</span>
+  
+  <p class="rightP">
+    <?php echo $this->LATEST_RECIPES; ?>
+  </p>
+
+</div>
+
+<br class="break" />
+
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+
+</div>
+<!-- End Body Area -->
---
+++ templates/search.tpl.php
@@ -0,0 +1,47 @@
+<!-- Start Body Area -->
+<div id="bodyArea">
+
+<div class="left">
+
+  <span class="headLeft"><?php echo $this->SEARCH_TXT; ?></span>
+
+  <span class="dateBar"><?php echo $this->SEARCH_RESULTS_TXT; ?></span>
+
+  <?php echo $this->SEARCH_RESULTS; ?>
+  
+  <?php
+  // Only show page numbers if recipe count higher than 0..
+  if ($this->COUNT>0) {
+  ?>
+  <p class="pages"><?php echo $this->PAGE_NUMBERS; ?></p>
+  <?php
+  }
+  ?>
+</div>
+
+<div class="right">
+
+  <span class="headRight"><?php echo $this->FILTER_BY_TXT; ?>:</span>
+  
+  <p class="rightP">
+    <select onchange="if(this.value!= 0){location=this.options[this.selectedIndex].value}">
+    <option value="0">Pilih Provinsi</option>
+    <?php echo $this->LOAD_CATEGORIES; ?>
+    </select>
+  </p>
+  
+  <!--<span class="headRightMid"><?php echo $this->MOST_POPULAR_TXT; ?>:</span>
+  
+  <p class="rightP">
+    <?php echo $this->MOST_POPULAR; ?>
+  </p>-->
+
+</div>
+
+<br class="break" />
+
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+
+</div>
+<!-- End Body Area -->
